33798bc1b3cea0bd8e2defeaf54c988c5f46a72a
Viscus <keegansaint194@hotmail.com>
2016-10-04 11:20:25 -0400

    added basic log in functionality

---
+++ src/com/theironyard/Hurricane.java
@@ -0,0 +1,23 @@
+package com.theironyard;
+
+/**
+ * Created by Viscus on 10/4/16.
+ */
+public class Hurricane {
+    enum    Category{
+        ONE, TWO, THREE, FOUR, FIVE
+    }
+
+
+    String name;
+    String location;
+    Category category;
+    String image;
+
+    public Hurricane(String name, String location, Category category, String image) {
+        this.name = name;
+        this.location = location;
+        this.category = category;
+        this.image = image;
+    }
+}
